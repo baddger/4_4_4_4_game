0a05e8442acf10d179dcbd093dc22a85d027b6fe
baddger <start.964@gmail.com>
2026-08-22 16:23:34 +0200

    premiere modif de claude

--- player.gd
+++ player.gd
@@ -1,62 +1,57 @@
 extends Node3D
-const move_dist : float = 1
-const move_frame_count : float = 10
-const dist_by_frame : float = move_dist / move_frame_count
 
-enum state_player {NOP, PLAYER1, PLAYER2, PLAYER3, PLAYER4}
-var state = state_player.NOP
+const MOVE_DISTANCE := 1.0
+const MOVE_FRAMES := 10
+const DIST_PER_FRAME := MOVE_DISTANCE / MOVE_FRAMES
 
+enum Direction { NONE, UP, DOWN, LEFT, RIGHT }
 
-const STATE_DIRECTIONS := {
-	state_player.NOP: Vector3.ZERO,
-	state_player.PLAYER1: Vector3.BACK,    # (0, 0, -1)
-	state_player.PLAYER2: Vector3.FORWARD, # (0, 0, 1)
-	state_player.PLAYER3: Vector3.RIGHT,   # (1, 0, 0)
-	state_player.PLAYER4: Vector3.LEFT,    # (-1, 0, 0)
+const DIRECTION_VECTORS := {
+	Direction.UP: Vector3.BACK,      # (0, 0, -1)
+	Direction.DOWN: Vector3.FORWARD, # (0, 0, 1)
+	Direction.LEFT: Vector3.RIGHT,   # (1, 0, 0)
+	Direction.RIGHT: Vector3.LEFT,   # (-1, 0, 0)
 }
 
-const STATE_ROTATIONS := {
-	state_player.NOP: null,
-	state_player.PLAYER1: 1.0 * PI,
-	state_player.PLAYER2: 2.0 * PI,
-	state_player.PLAYER3: 1.5 * PI,
-	state_player.PLAYER4: 0.5 * PI,
+const DIRECTION_ROTATIONS := {
+	Direction.UP: 1.0 * PI,
+	Direction.DOWN: 2.0 * PI,
+	Direction.LEFT: 1.5 * PI,
+	Direction.RIGHT: 0.5 * PI,
 }
 
-func _ready():
-	pass
+const INPUT_TO_DIRECTION := {
+	"ui_up": Direction.UP,
+	"ui_down": Direction.DOWN,
+	"ui_left": Direction.LEFT,
+	"ui_right": Direction.RIGHT,
+}
 
-var cpt = 0
-var prev_rot = 0
-func _physics_process(delta: float) -> void:
+var _direction := Direction.NONE
+var _start_rotation := 0.0
+var _frame := 0
 
-	if state == state_player.NOP :
-		if Input.is_action_just_pressed("ui_up") :
-			state = state_player.PLAYER1
-		if Input.is_action_just_pressed("ui_down") :
-			state = state_player.PLAYER2
-		if Input.is_action_just_pressed("ui_left") :
-			state = state_player.PLAYER3
-		if Input.is_action_just_pressed("ui_right") :
-			state = state_player.PLAYER4
-		if state != state_player.NOP :
-			prev_rot = rotation.y 
-			
-	else :
-		
-		if cpt == move_frame_count :
-			cpt = 0
-			
-			state = state_player.NOP
+func _physics_process(_delta: float) -> void:
+	if _direction == Direction.NONE:
+		_read_input()
+	else:
+		_move_step()
+
+func _read_input() -> void:
+	for action in INPUT_TO_DIRECTION:
+		if Input.is_action_just_pressed(action):
+			_direction = INPUT_TO_DIRECTION[action]
+			_start_rotation = rotation.y
 			return
-		else :
-			var step = abs(prev_rot - STATE_ROTATIONS[state]) / move_frame_count
-			if step != 0 :
-				#var values = range(prev_rot, STATE_ROTATIONS[state], step)
-				rotation.y += step
-				rotation.y = fmod(rotation.y, TAU)
-			
-			position += STATE_DIRECTIONS[state] * dist_by_frame
-			position = position.snapped(Vector3.ONE * 0.001)
-			print(rotation.y)
-			cpt += 1
+
+func _move_step() -> void:
+	var step = abs(_start_rotation - DIRECTION_ROTATIONS[_direction]) / MOVE_FRAMES
+	rotation.y = fmod(rotation.y + step, TAU)
+
+	position += DIRECTION_VECTORS[_direction] * DIST_PER_FRAME
+	position = position.snapped(Vector3.ONE * 0.001)
+
+	_frame += 1
+	if _frame >= MOVE_FRAMES:
+		_frame = 0
+		_direction = Direction.NONE
