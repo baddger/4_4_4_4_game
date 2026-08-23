13ef5e798d42f902c284e792de18a2f24f4ded5e
baddger <start964@gmail.com>
2026-08-23 18:13:17 +0200

    camera

--- camera_zqsd_and_mouse.gd
+++ camera_zqsd_and_mouse.gd
@@ -1,1 +1,24 @@
 extends Camera3D
+
+## Node to keep centered in view. Defaults to the sibling node named "player".
+@export var target_path: NodePath
+
+var _target: Node3D
+var _z_offset: float
+
+
+func _ready() -> void:
+	if target_path.is_empty():
+		_target = get_parent().get_node_or_null("player")
+	else:
+		_target = get_node_or_null(target_path)
+
+	if _target:
+		_z_offset = global_position.z - _target.global_position.z
+	else:
+		push_warning("camera_zqsd_and_mouse: no target found to follow.")
+
+
+func _physics_process(_delta: float) -> void:
+	if _target:
+		global_position.z = _target.global_position.z + _z_offset
