f0172d61686c286ff8e8d103d1cd274c61642e99
baddger <start.964@gmail.com>
2026-08-23 13:53:18 +0200

    again !

--- pipe.gd
+++ pipe.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
-const CAR_SCENE = preload("res://car.tscn")
+const CAR_SCENE_BLUE = preload("res://car.tscn")
+const CAR_SCENE_RED = preload("res://car_red.tscn")
 const SPAWN_INTERVAL = 2.0
 
 enum PipeColor { BLUE, RED }
@@ -17,7 +18,12 @@ func _process(delta: float) -> void:
 
 	if _spawn_timer >= SPAWN_INTERVAL:
 		_spawn_timer = 0.0
-		var car_instance = CAR_SCENE.instantiate()
-		car_instance.position = position
+
+		var car_instance = null
+		if pipe_color == PipeColor.BLUE :
+			car_instance = CAR_SCENE_BLUE.instantiate()
+		if pipe_color == PipeColor.RED :
+			car_instance = CAR_SCENE_RED.instantiate()
+
 		car_instance.pipe_color = pipe_color
-		get_parent().add_child(car_instance)
+		add_child(car_instance)
