6ba0aad027b46feeb153282934ce2b4383759318
baddger <start964@gmail.com>
2026-08-23 20:52:57 +0200

    spawn car at start

--- pipe.gd
+++ pipe.gd
@@ -11,13 +11,22 @@ var _spawn_timer = 0.0
 
 
 func _ready() -> void:
-	pass
+
+	for car_number in range(0, 10, 1):
+		var car = spaawning()
+		for a in range(0, SPAWN_INTERVAL * car_number * Engine.physics_ticks_per_second, 1):
+			car._physics_process(1.0 / Engine.physics_ticks_per_second)
+
 
 func _process(delta: float) -> void:
 	_spawn_timer += delta
 
 	if _spawn_timer >= SPAWN_INTERVAL:
 		_spawn_timer = 0.0
-		var car_instance = CAR_SCENE.instantiate()
-		car_instance.pipe_color = pipe_color
-		add_child(car_instance)
+		spaawning()
+
+func spaawning() -> Node3D:
+	var car_instance = CAR_SCENE.instantiate()
+	car_instance.pipe_color = pipe_color
+	add_child(car_instance)
+	return car_instance
